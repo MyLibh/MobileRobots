538ed3d78251e452c345d7c9e97e43a8ec4b776b
mylibh <gav503244@gmail.com>
2020-09-16 04:41:09 +0300

    map_t renamed to pixmap_map_t

--- src/MobileRobots.hpp
+++ src/MobileRobots.hpp
@@ -4,8 +4,6 @@
 #include <QtWidgets/QMainWindow>
 #include <QTimer>
 
-#include <vector>
-
 #include "ui_MobileRobots.h"
 
 #include "Coord.hpp"
@@ -21,7 +19,7 @@ namespace MobileRobots
         Q_OBJECT
 
     private:
-        using map_t = std::unordered_map<Coord, QGraphicsPixmapItem*>;
+        using pixmap_map_t = std::unordered_map<Coord, QGraphicsPixmapItem*>;
         using scouts_map_t = std::vector<std::pair<QGraphicsPixmapItem*, std::shared_ptr<RobotScout>>>;
 
     private:
@@ -62,7 +60,7 @@ namespace MobileRobots
         std::shared_ptr<AI>                    m_ai;
         std::unique_ptr<QTimer>                m_timer;
         std::unique_ptr<QGraphicsScene>        m_scene;
-        map_t                                  m_map;
+        pixmap_map_t                           m_map;
         scouts_map_t                           m_scouts;
         std::map<std::string, QPixmap>         m_images;
         Coord                                  m_scaleFactor;
