69d3c52b2fe85a23733b85372bc4dbb3c9768362
mylibh <gav503244@gmail.com>
2020-09-17 19:06:28 +0300

    fixed menu bar height

--- src/MobileRobots.hpp
+++ src/MobileRobots.hpp
@@ -23,7 +23,7 @@ namespace MobileRobots
 
     private:
         inline static constexpr auto INFO_WIDTH{ 200U }; //-V112
-        inline static constexpr auto MENU_HEIGHT{ 26 }; //-V112
+        inline static constexpr auto MENU_HEIGHT{ 21 }; //-V112
         inline static constexpr auto TIMER_INTERVAL_DEFAULT{ 150ms };
         inline static constexpr auto TIMER_INTERVAL_STEP{ 25ms };
         inline static constexpr auto DEFAULT_CONFIG_NAME{ ":/cfg/map.json" };
